aaa9b22d82abe59fee2adb4e72db4ba4e48c62d0
xthecoolboy <67747753+xthecoolboy@users.noreply.github.com>
2020-07-14 16:40:47 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,14 +1,33 @@
-const { MessageEmbed } = require("discord.js");
-module.exports = {
-  name: "say",
-  aliases: ["bc", "broadcast"],
-  execute(message, args, client) {
-    const embed = new MessageEmbed()
-      .setTitle(message.author.username)
-      .setColor(client.colors.blue)
-      .setFooter("Created By Bumpy")
-      .setThumbnail(message.author.avatarURL())
-      .setDescription(args.join(" "));
+const Discord = require('discord.js');
+module.exports.run = async (client, message, args) => {
+  if (args == 'help') {
+    let embed = new Discord.RichEmbed()
+      .setTitle(`${module.exports.help.name} Command Information`)
+      .setDescription(`${module.exports.help.description}`)
+      .addField('Usage', `${config.prefix}${module.exports.help.usage}`, true)
+      .addField('Permission', `${module.exports.help.permission}`, true)
+      .setColor(config.cyan)
     message.channel.send(embed);
-  },
+    return
+  };
+
+  if (!message.member.hasPermission(`${module.exports.help.permission}`)) return errors.noPermissions(message, `${module.exports.help.permission}`);
+
+  if (!args[0]) return errors.emptyMessage(message);
+  let inputmessage = args.join(' ');
+
+  let embed = new Discord.RichEmbed()
+    .setColor(config.green)
+    .setDescription(inputmessage)
+
+  message.delete().catch(O_o => {});
+  message.channel.send(embed);
+  return
+};
+
+module.exports.help = {
+  name: 'say',
+  description: 'This will send a message as the bot on your behalf.',
+  permission: 'ADMINISTRATOR',
+  usage: 'say [content]'
 };
