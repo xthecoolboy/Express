e7b5c05a4a6b97e82799ea754fe115369e1ba824
xthecoolboy <67747753+xthecoolboy@users.noreply.github.com>
2020-07-14 15:50:17 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,18 +1,14 @@
-const Discord = require("discord.js");
-
-exports.run = (bot, message, args, func, cmd) => {
-    if (args[0] == null) {
-      const erro = new Discord.MessageEmbed()
-        .setTitle("Algo deu errado!")
-        .setDescription("Você esqueceu de informar o que eu vou dizer! Veja abaixo, alguns exemplos de como usar o comando `Say`")
-        .addField('**Sintaxe:**', 'la/say `Mensagem`')
-        .addField('**Exemplos**', 'la/say `Eu sou muito fofa :3`')
-        .setColor('#2A1250')
-        message.channel.send(erro)
-    } else if (args[0] != 0) {
-        const sayMessage = (args.join(" "))
-        const m = (sayMessage.replace("Sebola", `<@${message.author.id}>`).replace("sebola", `<@${message.author.id}>`))
-        message.delete().catch(O_o => {});
-        message.channel.send(m);
-    }
-}
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+  name: "say",
+  aliases: ["bc", "broadcast"],
+  execute(message, args, client) {
+    const embed = new MessageEmbed()
+      .setTitle(message.author.username)
+      .setColor(client.colors.blue)
+      .setFooter("Created By Bumpy")
+      .setThumbnail(message.author.avatarURL())
+      .setDescription(args.join(" "));
+    message.channel.send(embed);
+  },
+};
