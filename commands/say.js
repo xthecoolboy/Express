2e09b275702b54be39b05dc9fc197c7cc55cfe0f
xthecoolboy <67747753+xthecoolboy@users.noreply.github.com>
2020-07-14 15:18:12 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,9 +1,19 @@
-exports.run = (client, message, args, ops) => {
 const Discord = require('discord.js')
 
 exports.run = function(bot, message, args){
+    let sentmessage = args.splice(1).join(' ');
+    let user = message.mentions.users.first();
+    if (message.mentions.users.size < 1) return message.channel.send("You must mention the user you are going to message.").catch(console.error);
+    if (message.length < 1) return message.channel.send('Please enter a message.');
     const embed = new Discord.RichEmbed()
-    .setTitle(args.join(" "))
+    .setAuthor(`New message from ${message.author.tag}`, message.author.avatarURL)
     .setColor(message.guild.me.displayHexColor)
-    message.channel.send({embed, embed})
+    .addField('Message', args.join(" "))
+    message.channel.send(`Message sent to ${user.tag}`)
+    const logembed = new Discord.RichEmbed()
+    .setAuthor('New message!', message.author.avatarURL)
+    .addField('User', message.author.avatarURL)
+    .addField('Message', args.join(" "))
+    .addField('To', user.tag)
+    bot.channels.get('369125980515860480').send({logembed, logembed})
 }
